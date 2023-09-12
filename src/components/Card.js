70f2f998f9964b6f2277035d9811abed979c3f9b
Nabin Panthi <panthinabin7@gmail.com>
2023-09-12 14:06:13 +0545

    backend connected to front-end

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,14 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
+  let options = props.options;
+  let priceOptions = Object.keys(options);
+
   return (
     <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
-      <img
-        src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1780&q=80"
-        className="card-img-top"
-        alt="..."
-        height={200}
-      />
+      <img src={props.imgSrc} className="card-img-top" alt="..." height={200} />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
+        <h5 className="card-title">{props.foodName}</h5>
         <p className="card-text">This is some Important text</p>
         <div className="container w-100">
           <select className="m-2 h-100  bg-success rounded">
@@ -24,8 +22,13 @@ const Card = () => {
           </select>
 
           <select className="m-2 h-100  bg-success rounded">
-            <option value="half">Half</option>
-            <option value="full">Full</option>
+            {priceOptions
+              .filter((data) => data !== "_id") // Filter out "_id"
+              .map((data) => (
+                <option key={data} value={data}>
+                  {data}
+                </option>
+              ))}
           </select>
 
           <div className="d-inline h-100 fs-5">Total Price</div>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
-import Carousal from "../components/Carousal";
 
 const Home = () => {
+  const [search, setSearch] = useState("");
   const [foodCat, setFoodCat] = useState([]);
   const [foodItem, setFoodItem] = useState([]);
 
@@ -26,32 +26,120 @@ const Home = () => {
     loadData();
   }, []);
 
-  console.log(foodCat);
-
   return (
     <div>
       <div>
         <Navbar />
       </div>
-      <div>
-        <Carousal />
+      {/* crousal */}
+      <div
+        id="carouselExampleFade"
+        className="carousel slide carousel-fade"
+        data-bs-ride="carousel"
+      >
+        <div
+          className="carousel-inner"
+          id="carousel"
+          style={{ objectFit: "contain !important" }}
+        >
+          <div className="carousel-caption" style={{ zIndex: "10" }}>
+            <div className="d-flex justify-content-center">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+              />
+              {/* <button
+                className="btn btn-outline-success text-white bg-success"
+                type="submit"
+              >
+                Search
+              </button> */}
+            </div>
+          </div>
+          <div className="carousel-item active">
+            <img
+              src="https://source.unsplash.com/random/900×700/?pizza"
+              className="d-block w-100"
+              style={{ filter: "brightness(30%)" }}
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://source.unsplash.com/random/900×700/?momos"
+              className="d-block w-100"
+              style={{ filter: "brightness(30%)" }}
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://source.unsplash.com/random/900×700/?burger"
+              className="d-block w-100"
+              style={{ filter: "brightness(30%)" }}
+              alt="..."
+            />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
+      <div>{/* card */}</div>
       <div className="container">
         {foodCat.length !== 0 ? (
           foodCat.map((data, i) => {
             return (
-              <div>
+              <div className="row mb-3" key={data._id}>
                 <div key={data._id} className="fs-3 m-3">
                   {data.CategoryName}
                   <hr />
                 </div>
                 {foodItem.length !== 0 ? (
                   foodItem
-                    .filter((item) => item.CategoryName === data.CategoryName)
+                    .filter(
+                      (item) =>
+                        item.CategoryName === data.CategoryName &&
+                        item.name.toLowerCase().includes(search.toLowerCase())
+                    )
                     .map((filterItem) => {
                       return (
-                        <div key={filterItem._id}>
-                          <Card />
+                        <div
+                          key={filterItem._id}
+                          className="col-12 col-md-6 col-lg-3"
+                        >
+                          <Card
+                            foodName={filterItem.name}
+                            options={filterItem.options[0]}
+                            imgSrc={filterItem.img}
+                          />
                         </div>
                       );
                     })
@@ -64,7 +152,6 @@ const Home = () => {
         ) : (
           <div>********************</div>
         )}
-        <Card />
       </div>
       <div>
         <Footer />
